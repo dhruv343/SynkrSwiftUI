a6a6bfe673ec431fdcd71c54c4bcfd56221a7e1e
Dhruv Gupta <dhruv4258@gmail.com>
2025-03-06 11:19:52 +0530

    Initial Commit

---
+++ SynkrSwiftUI/SynkrSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynkrSwiftUIApp.swift
+//  SynkrSwiftUI
+//
+//  Created by Dhruv Gupta  on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SynkrSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
