0667b5f386e3ca73cc385485decac444b6785ae8
Dhruv Gupta <dhruv4258@gmail.com>
2025-03-11 23:19:36 +0530

    data model

---
+++ SynkrSwiftUI/Login.swift
@@ -0,0 +1,8 @@
+//
+//  Login.swift
+//  SynkrSwiftUI
+//
+//  Created by Dhruv Gupta  on 06/03/25.
+//
+
+import Foundation
---
+++ SynkrSwiftUI/SignUp.swift
@@ -0,0 +1,8 @@
+//
+//  SignUp.swift
+//  SynkrSwiftUI
+//
+//  Created by Dhruv Gupta  on 06/03/25.
+//
+
+import Foundation
